d49982f6ecde734bf0b6ea209c6cb8f7f67f788b
spectrumexperice <spectrumweb22@gmail.com>
2025-09-16 13:46:30 +0300

    updateURL

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-if(!process.env.MONGODB_URL){
+if(!process.env.MONGODATA_URL){
     throw new Error("please provide MONGODB URL")
 }
  async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODATA_URL)
         console.log("Connect DB ...")
     }catch(error){
         console.log("MONGODB connect error",error)
