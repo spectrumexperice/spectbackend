628893a23bf57badc3510b392474b36d87a25249
spectrumexperice <spectrumweb22@gmail.com>
2025-09-28 19:40:14 +0300

    updatedit

--- controller/sendMessage.js
+++ controller/sendMessage.js
@@ -1,5 +1,5 @@
 import MessageModel from '../models/messageModel.js'
-
+import sendEmail from '../config/sendEmail.js'
 const SendMessage=async (req,res)=>{
 try{
     const {fullName,email,phone,message}= req.body
@@ -13,11 +13,25 @@ try{
     const payload=({fullName,email,phone,message})
      const newMessage=await new MessageModel(payload)
      await newMessage.save()
+      await sendEmail({
+      to: "m.alhazmi@s-spectrum.com",
+      subject: `رسالة جديدة من ${fullName}`,
+      html: `
+        <p>لقد استلمت رسالة جديدة من الزبون:</p>
+        <p><strong>الاسم:</strong> ${fullName}</p>
+        <p><strong>البريد:</strong> ${email}</p>
+        <p><strong>الهاتف:</strong> ${phone}</p>
+        <p><strong>الرسالة:</strong></p>
+        <p>${message}</p>
+      `
+    });
+
      return res.status(201).json({
         message :"تم ارسال رسالتك بنجاح",
         error:false,
         success:true
      })
+     
 
 }catch(error){
     return res.status(500).json({
